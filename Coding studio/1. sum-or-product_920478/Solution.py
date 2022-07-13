c96ab0098db8b751eebc741d24d0646170e76fef
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2022-07-13 22:57:15 +0530

    Update Solution.py

--- Coding studio/1. sum-or-product_920478/Solution.py
+++ Coding studio/1. sum-or-product_920478/Solution.py
@@ -1,3 +1,4 @@
+# My solution !
 def sumOrProduct(n, q):
 
     # Write your Code here.
@@ -8,3 +9,33 @@ def sumOrProduct(n, q):
         for x in range(1,n+1):
             pro*=x
         return pro%((10**9) + 7 )
+
+# original solution 
+'''
+    Time Complexity : O(N)
+    Space Complexity : O(1)
+
+    Where 'N' is the number given.
+'''
+
+
+def sumOrProduct(n, q):
+
+    answer = 0
+    mod = int(1e9) + 7
+
+    if q == 1:
+
+        # Sum of first 'N' numbers is given by
+        # 'N' * 'N + 1' / 2.
+        answer = (n * (n + 1)) // 2
+
+    else:
+        answer = 1
+        for i in range(1, n + 1):
+            # Modular Arithmatic.
+            i %= mod
+            answer %= mod
+            answer = (((answer * i) % mod) + mod) % mod
+
+    return answer
